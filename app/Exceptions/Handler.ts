97fb32404f9650f9390d4dd25713a3e37613abcc
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-03-27 21:52:57 +0200

    feat: added error handler

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -15,9 +15,18 @@
 
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor() {
     super(Logger)
+  }
+  public async handle(error: any, ctx: HttpContextContract) {
+    const code = error.status ?? 500
+    const messages = error.messages?.errors?.map((e) => e.message) ?? [error.message]
+    Logger.info(error)
+    return ctx.response.status(code).send({
+      errors: code === 500 || !messages ? ['Error inesperado'] : messages,
+    })
   }
 }
